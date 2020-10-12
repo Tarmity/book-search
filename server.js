5201112a8740ba455c57eaeae03de56e533ee63d
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-12 16:12:38 +1100

    updated foler and file structure for whats required

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const router = require('./routes');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
@@ -14,6 +17,7 @@ if (process.env.NODE_ENV === "production") {
 // app.get("*", function(req, res) {
 //   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 // });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
 app.use(router);
 
