a13551eec453f0be7fba882ca553d120d3254136
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-10-12 19:52:11 +1100

    add the books model

---
+++ models/Books.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema({
+    googleId: { type: String, required: true, unique: true },
+    title: { type: String, required: true },
+    author: { type: [ String ] },
+    description: { type: String, required: true },
+    image: {type: String, required: true },
+    link: { type: String, required: true }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+
+module.exports = Book;
